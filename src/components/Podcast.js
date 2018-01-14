56bc911946e1d6d25e4a1c52aa42bfb0978353f5
James Edwards <jameshedwards@protonmail.com>
2018-01-14 13:49:58 -0500

    add podcast component

---
+++ src/components/Podcast.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import '../styles/Podcast.css';
+import { getPodcastInfo } from '../api/podcast.service';
+
+class Podcast extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            description: 'testing the description',
+            date: new Date(),
+            title: this.props.title
+        }
+    }
+
+    componentDidMount() {
+        this.setState( {uploader: 'jedwards'});
+        getPodcastInfo().then((payload) => {
+            console.log("info: ", payload)
+            this.setState({description: payload.data});
+        }).catch((err) => {
+            console.error(err)
+        })
+    }
+
+    render() {
+        return (
+            <div className="Podcast1">
+              <p>Title: {this.state.title}</p>
+              <p>Uploader: {this.state.uploader}</p>
+              <p>Description: {this.state.description}</p>
+              <p>Date Uploaded: {this.state.date.toLocaleString() }</p>
+              <p></p>
+            </div>
+        );
+    }
+}
+
+export default Podcast;
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,17 +2,20 @@ import React, { Component } from 'react';
 import logo from '../images/logo.svg';
 import '../styles/App.css';
 
+import Podcast from '../components/Podcast';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">react-podcaster</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p className="App-intro">a site to upload and listen to podcasts</p>
+        <div>
+          <Podcast title="TestTitle"/>
+        </div>
       </div>
     );
   }
